8a710dbe35704a8ed21563088b48487255fbb6aa
SedationH <sedationh@gmail.com>
2023-10-26 21:25:21 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
-function App() {
-  return <>This is App</>;
+import { useState } from "react"
+
+// 产生 1000 - 5000 的随机数
+function random() {
+  const min = 1000
+  const max = 5000
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min
+  return randomNumber
 }
 
-export default App;
+console.log("sedationh random", random())
+
+function request(value) {
+  return new Promise<{
+    data: string
+  }>((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: value,
+      })
+    }, random())
+  })
+}
+
+function SearchBox() {
+  const [result, setResult] = useState("")
+  const handleInput = (e) => {
+    const value = e.target.value
+    request(value).then((response) => {
+      setResult(response.data)
+    })
+  }
+  return (
+    <>
+      <input onChange={handleInput} />
+      {result}
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <SearchBox />
+    </>
+  )
+}
+
+export default App
