9a52a916bc44c7db098cd83816c80ccfad977f66
SedationH <sedationh@gmail.com>
2023-10-27 07:57:11 +0800

    7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useMemo, useState } from "react"
-import { BehaviorSubject, debounce, distinctUntilChanged, of, switchMap, timer } from "rxjs"
+import { useEffect, useMemo, useRef, useState } from "react"
+import { BehaviorSubject, Observable, debounce, distinctUntilChanged, of, switchMap, timer } from "rxjs"
 
 // 产生 4000 2000 1000 4000 ...
 const random2 = (function () {
@@ -24,10 +24,40 @@ function request(value) {
   })
 }
 
+function fetcher(input: string): Promise<{
+  value: string
+  error: boolean
+}> {
+  return new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) reject()
+      resolve("api " + input)
+    }, Math.random() * 1000)
+  })
+    .then((res) => {
+      return {
+        value: res,
+        error: false,
+      }
+    })
+    .catch(() => {
+      return {
+        value: "",
+        error: true,
+      }
+    })
+}
+
 function SearchBox() {
   const [result, setResult] = useState("")
   // 下面的 BehaviorSubject 和 Subject 一模一样，除了有一个初始值会在订阅时立刻发出
   const input$ = useMemo(() => new BehaviorSubject(""), [])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const errorRef = useRef<boolean>(false)
+  errorRef.current = error
+
   // 输入内容时向流发送值
   const handleInput = (e) => {
     input$.next(e.target.value)
@@ -37,7 +67,8 @@ function SearchBox() {
       .pipe(
         // 防抖的实现 -----------------------------
         debounce((input) => {
-          if (input.length === 0) {
+          // 补充 error 处理
+          if (input.length === 0 || errorRef.current) {
             return of(null) // 立即响应
           } else {
             return timer(500) // 等待 500ms
@@ -46,12 +77,30 @@ function SearchBox() {
         distinctUntilChanged(),
         // 网络请求的实现 -----------------------------
         switchMap((input) => {
-          return request(input) // 取最新结果
+          if (input.length === 0) {
+            setLoading(false)
+            setError(false)
+            return of({
+              value: "default",
+              error: false,
+            })
+          }
+          setError(false)
+          setLoading(true)
+          return fetcher(input)
         })
       )
-      .subscribe((v) => {
-        console.log("sedationh 1", v)
-        setResult(v.data)
+      .subscribe({
+        next: ({ error, value }) => {
+          if (error) {
+            setError(true)
+            setLoading(false)
+          } else {
+            setError(false)
+            setLoading(false)
+            setResult(value)
+          }
+        },
       })
     return () => {
       subscription.unsubscribe()
@@ -60,8 +109,10 @@ function SearchBox() {
 
   return (
     <>
-      <input onChange={handleInput} />
-      {result}
+      <div>
+        <input onChange={handleInput} />
+      </div>
+      {error ? "error" : loading ? "loading" : result}
     </>
   )
 }
